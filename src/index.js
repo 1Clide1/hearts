eae8f40fb3aa2cf10acabc8e65170f918f567556
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-03-23 16:59:44 -0400

    testing the dev service worker to see if this would work with github pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 // import service worker
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 // register service worker
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
 // get service worker for dev mode
-// import serviceWorker from "./swDev";
+import serviceWorker from "./swDev";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,4 +17,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 // run the new service worker
-// serviceWorker();
+serviceWorker();
