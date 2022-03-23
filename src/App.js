856d982d87fe6e26d0b7f32219aab2cf0ce30a74
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-03-23 16:41:03 -0400

    added the letter component to the app.js and tweaked the header to work properly and also added some animation to the load of the website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-
+import Letter from "./components/letter/letter";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="app-title">For My Love </h1>
       </header>
+      <Letter />
     </div>
   );
 }
